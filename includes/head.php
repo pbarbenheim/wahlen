e1e0671755e7583bfdd58eee483881ac2df4711f
Paul Barbenheim <paul@barbenheim.de>
2025-06-21 15:18:37 +0200

    move css into respective folder

--- includes/head.php
+++ includes/head.php
@@ -10,6 +10,6 @@ require_once "config.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo ($PAGE_TITLE ?? "Wahlen-Tool") ?></title>
-    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
